1557126cbb54212eded13966a6ea2215a88abaa9
stefscan <stephcharette@gmail.com>
2022-02-12 09:13:10 -0500

    Add 404 page to the theme

---
+++ 404.php
@@ -0,0 +1,18 @@
+<?php get_header(  ); ?>
+
+<div class="flex">
+
+    <section class="trois-quart">
+
+        <?php get_template_part( "templates/generic-loop" ) ?>
+
+    </section>
+    <section class="quart sidebar">
+
+        <?php get_sidebar(  ); ?>
+
+    </section>
+
+</div>
+
+<?php get_footer(  ); ?>
